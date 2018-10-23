38b48f03d9e04bdfc6fd08c1fbc5d18f3af4fa0d
Administrator <Administrator@W7OIRX5AGI0XJUM>
2018-10-23 20:31:20 +0800

    taskqueue

---
+++ src/main/java/com/gamewolf/dbcrawler/task/RedisTaskQueue.java
@@ -0,0 +1,19 @@
+package com.gamewolf.dbcrawler.task;
+
+import com.gamewolf.dbcrawler.model.base.Task;
+
+public class RedisTaskQueue implements TaskQueue {
+
+	@Override
+	public void addTask(Task task) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Task getTask() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
