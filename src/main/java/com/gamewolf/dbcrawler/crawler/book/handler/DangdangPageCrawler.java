676fd84b8e0cadb5c3e7cd5894cf6784d384d179
Administrator <Administrator@W7OIRX5AGI0XJUM>
2018-11-01 17:05:38 +0800

    ces

--- src/main/java/com/gamewolf/dbcrawler/crawler/book/handler/DangdangPageCrawler.java
+++ src/main/java/com/gamewolf/dbcrawler/crawler/book/handler/DangdangPageCrawler.java
@@ -61,9 +61,9 @@ public class DangdangPageCrawler extends ParameterizedInitializer{
 	}
 
 	private void run() {
-		String id=params.getStringValue("id");
+		Integer id=params.getIntegerValue("id");
 		if(id==null) {
-			id="19087";
+			id=19087;
 		}
 		String url="http://webapi.http.zhimacangku.com/getip?num=1&type=2&pro=&city=0&yys=0&port=1&pack="+id+"&ts=0&ys=0&cs=0&lb=1&sb=0&pb=4&mr=1&regions=";
 		ResultParser parser=new ResultParser() {
